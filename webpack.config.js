5fd0a7ab4d46042c3d6bedc427875ee899454a64
BaptisteJ24 <baptistejan@outlook.fr>
2023-03-31 16:44:41 +0200

    Javascript restructuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const glob = require("glob");
 
 module.exports = {
-    entry: glob.sync("./js/**/*.js"),
+    entry: glob.sync("./scripts/**/*.js"),
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "bundle.js"
